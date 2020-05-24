dffb3cf3218f4e977d383af51ff9756485090306
luckyleonard <dzhu31@hotmail.com>
2020-05-24 13:25:47 +1000

    ✨ feat(server) register all middlewares

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const Koa = require('koa');
+const Redis = require('ioredis');
 const next = require('next');
+const Router = require('koa-router');
+const session = require('koa-session');
+const koaBody = require('koa-body');
 
+const config = require('./config');
+const auth = require('./server/auth');
+
+const RedisSessionStore = require('./server/session-store');
+const redis = new Redis(config.redis);
+
+//初始化next render
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -8,10 +19,26 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = new Koa();
 
+  server.keys = ['cookie decrypt keys'];
+  const SESSION_CONFIG = {
+    key: 'sessionid',
+    maxAge: 24 * 60 * 60 * 1000,
+    store: new RedisSessionStore(redis),
+  };
+
+  server.use(koaBody());
+  server.use(session(SESSION_CONFIG, server));
+
+  //截获跟OAuth有关的请求
+  auth(server);
+
   server.use(async (ctx, next) => {
+    ctx.req.session = ctx.session;
+    //将session传给nextjs上下文
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
   });
+
   server.listen(3000, () => {
     console.log('server is running on port 3000');
   });
