adba55f4c8c5be3994edf2c9c5c7fcade75e971b
luckyleonard <dzhu31@hotmail.com>
2020-05-24 23:28:15 +1000

    ✨ feat(loading) achieve loading animation in _app with PageLoading
    - it will display according to loading state
    - state will change accord to the Router hooks
    - Route change after finishing getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,45 @@
 import App from 'next/app';
+import Router from 'next/router';
 import 'antd/dist/antd.css';
 import { Provider } from 'react-redux';
 import withRedux from '../utils/with-redux';
 import PageLayout from '../components/Layout';
+import PageLoading from '../components/PageLoading';
+import Link from 'next/link';
 
 class MyApp extends App {
-  //方法每次页面切换都会被执行,这个方法是重写了给pages页面组件使用的
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    }; //loading覆盖所有app下的页面
+  }
+
+  startLoading = () => {
+    this.setState({
+      loading: true,
+    });
+  };
+
+  stopLoading = () => {
+    this.setState({
+      loading: false,
+    });
+  };
+
+  componentDidMount() {
+    Router.events.on('routeChangeStart', this.startLoading);
+    Router.events.on('routeChangeComplete', this.stopLoading);
+    Router.events.on('routeChangeError', this.stopLoading);
+  }
+
+  componentWillUnmount() {
+    Router.events.off('routeChangeStart', this.startLoading);
+    Router.events.off('routeChangeComplete', this.stopLoading);
+    Router.events.off('routeChangeError', this.stopLoading);
+  }
+
+  //方法每次页面切换都会被执行,这个方法是重写了给pages页面组件使用的, 但会强制每个页面都服务器端渲染
   static async getInitialProps(ctx) {
     const { Component } = ctx;
     let pageProps = {};
@@ -18,10 +52,15 @@ class MyApp extends App {
   //重写App的render方法
   render() {
     const { Component, pageProps, reduxStore } = this.props;
+    console.log(this.state.loading);
     //Component指代的就是pages文件夹下被渲染的页面组件
     return (
       <Provider store={reduxStore}>
+        {this.state.loading ? <PageLoading /> : null}
         <PageLayout>
+          <Link href='/detail'>
+            <a> detail </a>
+          </Link>
           <Component {...pageProps} />
         </PageLayout>
       </Provider>
