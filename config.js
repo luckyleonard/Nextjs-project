cd7cdbe32cb20daf907ec0b892317935abc5c586
luckyleonard <dzhu31@hotmail.com>
2020-05-26 20:55:39 +1000

    🐛 fix(.env) fix the compile bug with dotenv
    - const {} = process.env can't get environment varible at unknow reason
    * fix the issue with use process.env.CLIENT_ID to get the value

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
-const { CLIENT_ID, CLIENT_SECRET } = process.env;
+// console.log('from config', process.env);
+// const { CLIENT_ID, CLIENT_SECRET } = process.env;
+// console.log('clinetid:', CLIENT_ID, 'clinetsecret:', CLIENT_SECRET);
 const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize';
 const SCOPE = 'user';
 
 module.exports = {
   github: {
+    github_base_url: 'https://api.github.com',
     request_token_url: 'https://github.com/login/oauth/access_token',
-    client_id: CLIENT_ID,
-    client_secret: CLIENT_SECRET,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
   },
   redis: {
     port: 6379,
     host: '192.168.99.100',
   },
   GITHUB_OAUTH_URL,
-  OAUTH_URL: `${GITHUB_OAUTH_URL}?client_id=${CLIENT_ID}&scope=${SCOPE}`,
+  OAUTH_URL: `${GITHUB_OAUTH_URL}?client_id=${process.env.CLIENT_ID}&scope=${SCOPE}`,
 };
