faab5d8e8d31eebe32afb1d1863cb0a27880d58b
luckyleonard <dzhu31@hotmail.com>
2020-06-02 11:10:37 +1000

    📦 file(npm) install bundleAnalyzer for build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // const dotenvLoad = require('dotenv-load');
 // dotenvLoad();
-
+const webpack = require('webpack');
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withCss = require('@zeit/next-css');
 const config = require('./config');
 
@@ -53,9 +54,26 @@ if (typeof require !== 'undefined') {
 //     staticFolder: '/static',
 //   },
 // };
-module.exports = withCss({
-  publicRuntimeConfig: {
-    GITHUB_OAUTH_URL: config.GITHUB_OAUTH_URL,
-    OAUTH_URL: config.OAUTH_URL,
-  },
-});
+module.exports = withBundleAnalyzer(
+  withCss({
+    publicRuntimeConfig: {
+      GITHUB_OAUTH_URL: config.GITHUB_OAUTH_URL,
+      OAUTH_URL: config.OAUTH_URL,
+    },
+    webpack(config) {
+      config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+      return config;
+    },
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/server.html',
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../bundles/client.html',
+      },
+    },
+  })
+);
